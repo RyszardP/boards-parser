193abb4371f189134f07550399620681810bc7de
Rysard Poplavskij <rpoplavskij@gmail.com>
2021-04-30 00:44:06 +0300

    refactor

--- src/main/java/io/ryszardp/parser/YandexMarketParser.java
+++ src/main/java/io/ryszardp/parser/YandexMarketParser.java
@@ -2,6 +2,8 @@ package io.ryszardp.parser;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
@@ -9,10 +11,47 @@ import java.net.URL;
 public class YandexMarketParser {
 
     private static Document getPage(String url) throws IOException {
-        Document page = Jsoup.parse(new URL(url), 15000);
+        // Document page = Jsoup.parse(new URL(url), 15000);
+       // Document page = Jsoup.parse(new URL(url), 25000);
+        Document page = Jsoup.connect(url).maxBodySize(Integer.MAX_VALUE).get();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return page;
     }
 
-    private static final String MAIN = "http://www.nastolki.by/2-%D0%B8-%D0%B1%D0%BE%D0%BB%D0%B5%D0%B5-%D0%B8%D0%B3%D1%80%D0%BE%D0%BA%D0%BE%D0%B2-page-";
+    private static final String MAIN = "https://market.yandex.by/catalog--nastolnye-igry-v-minske/59742/list?hid=10682647&track=pieces&onstock=1&local-offers-first=0";
+    private static final String href = "https://market.yandex.by/";
 
+    public static void main(String[] args) throws Exception {
+        System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
+        int pageNumbers = 3;
+        Document page = getPage(MAIN);
+        // System.out.println(page);
+
+
+        // body > div._111XIPXNiH.main > div:nth-child(4) > div.tamefSzMtq > div:nth-child(4)
+        // body > div._111XIPXNiH.main > div:nth-child(4) > div.tamefSzMtq > div:nth-child(4) > div > div:nth-child(1) > div > div > div
+        Element tableCont = page.select("body").first();
+       //System.out.println(tableCont);
+        Elements divs = page.select("article");
+       // System.out.println(divs);
+        for (Element div : divs) {
+         //   System.out.println(div);
+            String gameLink = div.select(" a").attr("href");
+            System.out.println(gameLink);
+        }
+        // Element table = page.getElementById()
+        //     System.out.println(tableCont);
+//         Elements itemCells = tableCont.select("article");
+
+        //   for (Element element : itemCells) {
+        //     String gameLink = element.select("a").attr("href");
+
+        //   System.out.println(gameLink);
+        // }
+    }
 }
